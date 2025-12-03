e60d9e2b2fa0e8cc435e8513c283fe372a9013ae
RivaldiKC <aldiubun@gmail.com>
2025-12-03 20:14:20 +0700

    menambahkan semua file aplikasi medication information

---
+++ obat/panadol.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Halaman utama</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style2.css">
+  </head>
+  <body>
+
+
+
+    <nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand text-white" href="#">Hai Dok!</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="../index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">About</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-info" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+
+<div class="card" style="width: 50rem; margin-left:20%;">
+  <img src="../img/panadol.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+   <h3>Deskripsi</h3>
+  <p>
+PANADOL EXTRA merupakan obat dengan kandungan Paracetamol dan Caffeine. Obat ini dapat digunakan untuk meringankan sakit kepala dan sakit gigi. Paracetamol sebagai analgetik, bekerja dengan meningkatkan ambang rasa sakit dan Caffeine bekerja dengan menghambat kerja reseptor adenosin sehingga mengurangi rasa nyeri.
+Indikasi Umum</p> <hr>
+
+<h3>Indikasi Umum</h3>
+<p>Obat ini digunakan untuk meringankan sakit kepala dan sakit gigi.</p><hr>
+<h3>Komposisi</h3>
+<p>Tiap kaplet mengandung Paracetamol 500 mg dan Caffeine 65 mg.</p><hr>
+
+<h3>Dosis</h3>
+
+<p>Dewasa dan anak-anak lebih dari 12 tahun : 1 Kaplet, ditelan dengan segelas air, 3-4 kali sehari bila gejala memburuk. Tidak melebihi 8 kaplet dalam 24 jam. Minimum interval penggunaan dosis adalah 4 jam.</p><hr>
+<h3>Aturan Pakai</h3>
+<p>Sebelum atau sesudah makan.
+Kontra Indikasi
+Wanita hamil dan menyusui. Tidaj dianjurkan untuk digunakan pada anak dibawah usia 12 tahun
+Perhatian
+
+Hati-hati penggunaan obat ini pada penderita penyakit ginjal. Bila setelah 2 hari demam tidak menurun atau setelah 5 hari nyeri tidak menghilang, segera hubungi Unit Pelayanan Kesehatan. Penggunaan obat ini pada penderita yang mengkonsumsi alkohol, dapat meningkatkan risiko kerusakan fungsi hati. Kategori kehamilan : Kategori C: Mungkin berisiko. Obat digunakan dengan hati-hati apabila besarnya manfaat yang diperoleh melebihi besarnya risiko terhadap janin. Penelitian pada hewan uji menunjukkan risiko terhadap janin dan belum terdapat penelitian langsung terhadap wanita hamil.</p><hr>
+
+<h3>Efek Samping</h3>
+
+<p>Pemakaian obat umumnya memiliki efek samping tertentu dan sesuai dengan masing-masing individu. Jika terjadi efek samping yang berlebih dan berbahaya, harap konsultasikan kepada tenaga medis. Penggunaan jangka panjang dan dosis besar dapat menyebabkan kerusakan hati. Efek samping lain yaitu reaksi hipersensitifitas seperti kemerahan atau gatal pada kulit, kulit terkelupas, kadang-kadang gangguan pernafasan atau bengkak pada bibir, lidah, tenggorokan, sariawan, memar-memar, pendarahan. Namun, reaksi efek samping jarang terjadi. Hentikan penggunaan obat dan segera hubungi dokter jika mengalami efek samping.</p>
+
+<br>
+  <br>
+
+
+  <a href="../utama.php" type="button" class="btn btn-dark">Kembali</a>
+
+  </div>
+</div>
+
+
+  
+  
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ utama.php
@@ -0,0 +1,124 @@
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Halaman utama</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style2.css">
+  </head>
+  <body>
+
+
+
+    <nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand text-white" href="#">Hai Dok!</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">About</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-info" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+   <!-- <div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+    The current link item
+  </a>
+  <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+  <a href="#" class="list-group-item list-group-item-action">A third link item</a>
+  <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
+  <a class="list-group-item list-group-item-action">A disabled link item</a>
+</div> -->
+
+<ul>
+  <li>
+    <div class="card" style="width: 18rem;">
+  <img src="img/panadol.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Panadol Extra</h5>
+    <p class="card-text">Obat Sakit Kepala</p>
+    <a href="obat/panadol.php" class="btn btn-primary">Lebih Detail</a>
+  </div>
+</div>
+</li>
+
+<li>
+<div class="card" style="width: 18rem;">
+  <img src="img/oskadon.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Oskadon</h5>
+    <p class="card-text">Obat Sakit Kepala</p>
+    <a href="#" class="btn btn-primary">Lebih Detail</a>
+  </div>
+</div>
+</li>
+
+<li>
+<div class="card" style="width: 18rem;">
+  <img src="img/komix.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Komix OBH</h5>
+    <p class="card-text">Obat Batuk</p>
+    <a href="#" class="btn btn-primary">Lebih Detail</a>
+  </div>
+</div>
+</li>
+
+<li>
+<div class="card" style="width: 18rem;">
+  <img src="img/promag.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Promagh</h5>
+    <p class="card-text">Obat Sakit Magh</p>
+    <a href="#" class="btn btn-primary">Lebih Detail</a>
+  </div>
+</div>
+</li>
+
+<li>
+<div class="card" style="width: 18rem;">
+  <img src="img/inza.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">INZA</h5>
+    <p class="card-text">Obat Flu</p>
+    <a href="#" class="btn btn-primary">Lebih Detail</a>
+  </div>
+</div>
+</li>
+
+<li>
+<div class="card" style="width: 18rem;">
+  <img src="img/entrostop.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Entrostop</h5>
+    <p class="card-text">Obat Diare</p>
+    <a href="#" class="btn btn-primary">Lebih Detail</a>
+  </div>
+</div>
+</li>
+
+
+
+</ul>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  </body>
+</html>
